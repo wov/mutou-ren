a1aadb37b982c34edcd72042482d42cdc3ef292f
kavin <lenye01@gmail.com>
2012-06-10 15:10:49 +0800

    #

--- app-server/app.js
+++ app-server/app.js
@@ -131,8 +131,8 @@ sockets.on('connection',function(socket){
 				}
 				if(role_in_list(sessionID,gameParams)){
 					socket.emit("raiseException",-2);
-					socket.emit("reloadStage",JSON.stringify(gameParams));
 					socket.emit("sendCurrentSessionID",sessionID);
+					socket.emit("reloadStage",JSON.stringify(gameParams));
 					return;
 					}
 				}
