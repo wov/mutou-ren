62105a4d0fe7907c0308305f1c85511dfa0aaa21
wov <wovfeng@gmail.com>
2012-07-01 13:28:11 +0800

    change cfg.js

--- static/script/cfg.js
+++ static/script/cfg.js
@@ -13,16 +13,23 @@ Role.current = null;
 Role.id = null;//只有木头人才有的id
 Role.currentSessionID = null; //刷新后重新获取的sessionID
 
-<<<<<<< HEAD
 
 //客户端监听的接口定义
 //character : 角色 [1p-主机，all-所有的,system-系统的]
+
 //statge : 分为 loading，choose，select，main,finish
-//loading为刚载入游戏。
-//choose 为选择游戏 --单机游戏，联机游戏，创建房间，加入房间等
-//select 为选择角色
-//main 为游戏场景
-//finish 为游戏结束
+    //loading为刚载入游戏。
+    //choose 为选择游戏 --单机游戏，联机游戏，创建房间，加入房间等
+    //select 为选择角色
+    //main 为游戏场景
+    //finish 为游戏结束
+
+
+//全局定义。默认返回的参数格式为json。
+//{
+// code : 1,//1为正常情况，-1为异常情况。其他返回码自定义
+// data : ''//返回的东西根据实际情况。
+// }
 
 var INTERFACES_ON = {
     'connect' : {
@@ -30,14 +37,14 @@ var INTERFACES_ON = {
         'role' : '',
         'desc' : '默认事件，当与服务器连上之后会立即触发。',
         'statge' : 'loading',
-        'broadcast' : false
+        'broadcast' : false,
     },
     'playerNum' : {
         'character' : '',
         'role' : '',
         'desc' : '获得当前连接到服务器的人数',
         'statge' : 'choose',
-        'broadcast' : true
+        'broadcast' : true,
     },
     'roomNum' : {
         'character' : '',
@@ -92,19 +99,14 @@ var INTERFACES_EMIT = [
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-=======
 //可选角色。
 //为数组分别是
 var selectableRole = [false,true,true,true];
->>>>>>> 75e1926cb0fb784ef9d3dec94b123dfec9822282
+
+
+
+//前端可以调用的接口。
+
+
+
+
