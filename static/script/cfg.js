3104fac85b4bfd3f6ce9b53e3b9ce98282c19f49
wov <wovfeng@gmail.com>
2012-07-02 22:18:36 +0800

    fix the bug @cfg.js

--- static/script/cfg.js
+++ static/script/cfg.js
@@ -67,7 +67,7 @@ var INTERFACES_ON = {
         'desc' : '默认事件，当与服务器断开连接时触发。',
         'broadcast' : false
     },
-    'addPeople' : {
+    'addPerson' : {
         'character' : 'all',
         'role' : 'all',
         'statge' : 'main',
